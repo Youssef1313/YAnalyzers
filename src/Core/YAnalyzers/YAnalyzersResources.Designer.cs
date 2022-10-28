99f404721132dbbecb834a8b248bdf341eb561be
Youssef1313 <youssefvictor00@gmail.com>
2022-10-28 05:42:25 +0200

    Fix description to end with punctuation

--- src/Core/YAnalyzers/YAnalyzersResources.Designer.cs
+++ src/Core/YAnalyzers/YAnalyzersResources.Designer.cs
@@ -19,7 +19,7 @@ namespace YAnalyzers {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class YAnalyzersResources {
@@ -61,7 +61,7 @@ namespace YAnalyzers {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Variables should use explicit type when type is not apparent.
+        ///   Looks up a localized string similar to Variables should use explicit type when type is not apparent..
         /// </summary>
         internal static string UseExplicitTypeDescription {
             get {
@@ -88,7 +88,7 @@ namespace YAnalyzers {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Variables should use implicit type when type is apparent.
+        ///   Looks up a localized string similar to Variables should use implicit type when type is apparent..
         /// </summary>
         internal static string UseImplicitTypeDescription {
             get {
